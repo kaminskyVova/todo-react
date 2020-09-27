ba7c2558cea8ad9269cf93877eea3110a6059b15
unknown <kamok@bk.ru>
2020-09-27 04:34:12 +0300

    Updates

---
+++ precache-manifest.35d9d093bf3fe50929b70966de9d9a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbe088eb10b878cf0b02a21fa8d14c2",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "e8ea36cba62eaef0454c",
+    "url": "/todo-react/static/css/main.9c8f7a5a.chunk.css"
+  },
+  {
+    "revision": "4593b33ceacd8cbb7649",
+    "url": "/todo-react/static/js/2.8971845c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.8971845c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ea36cba62eaef0454c",
+    "url": "/todo-react/static/js/main.2eeeb8f7.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
